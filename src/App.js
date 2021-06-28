304377b53276df4b84c197f7c2c6ef8102068587
Jeimissonb <jeimissonb@hotmail.com>
2021-06-28 18:36:06 -0300

    Created form for add employee, and buttons interactions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
             <HeaderComponent/>
               <div className="container">
                 <Switch>
-                  <Route path="/" component={ListEmployeeComponent} />
+                  <Route path="/" exact component={ListEmployeeComponent} />
                   <Route path="/employees" component={ListEmployeeComponent} />
+                  <Route path="/add-employee" component={CreateEmployeeComponent} />
                   
                   <ListEmployeeComponent />
                 </Switch> 
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URS = "http://localhost:8080/api/v1/employees"
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees"
 
 class EmployeeService {
 
     getEmployees(){
-        return axios.get(EMPLOYEE_API_BASE_URS);
+        return axios.get(EMPLOYEE_API_BASE_URL);
 
     }
 
