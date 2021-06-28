6cd890716068704973a7b35e85e4a06a92ff7b5c
Jeimissonb <jeimissonb@hotmail.com>
2021-06-28 11:40:29 -0300

    Fist commit, created services, employeelist component request based on axios promisse

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URS = "http://localhost:8080/api/v1/employees"
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URS);
+
+    }
+
+}
+
+export default new EmployeeService();
