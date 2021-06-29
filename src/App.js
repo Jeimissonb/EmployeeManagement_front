e0002814e5657124b5ee63e07c482bf8b5614bff
Jeimissonb <jeimissonb@hotmail.com>
2021-06-28 22:37:25 -0300

    Connecting update employee with backend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
                   <Route path="/" exact component={ListEmployeeComponent} />
                   <Route path="/employees" component={ListEmployeeComponent} />
                   <Route path="/add-employee" component={CreateEmployeeComponent} />
+                  <Route path="/update-employee/:id" component={UpdateEmployeeComponent} />
+
                   
                   <ListEmployeeComponent />
                 </Switch> 
